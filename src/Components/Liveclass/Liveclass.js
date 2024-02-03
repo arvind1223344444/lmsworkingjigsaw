acc4c5e4267f054d59cd96a25054c9cd2b825df5
arvindrawatweb <arvindrawat5595@gmail.com>
2024-02-03 11:40:55 +0530

    first

--- src/Components/Liveclass/Liveclass.js
+++ src/Components/Liveclass/Liveclass.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Liveclass() {
   return (
     <>
-    <iframe width="560" height="400" src="https://agora-kappa.vercel.app/"  allow="camera; microphone;speaker" ></iframe>
+
+    <iframe width="560" height="400" src="https://jigsawabacuslivestemaing.vercel.app/"  allow="camera; microphone;speaker" ></iframe>
+    {/* <iframe width="560" height="1000" src="https://agora-kappa.vercel.app/" allow="camera; microphone; speaker"></iframe> */}
+
     </>
   )
 }
